81d1aa82d03cb8c39515be99e8f5929c4bf4feeb
Jacky Alcine <yo@jacky.wtf>
2015-08-06 02:34:26 -0400

    closest to the solution as I'd get.

--- app.rb
+++ app.rb
@@ -5,13 +5,14 @@ class BrandFactoryRecommender
   attr_accessor :minimum
 
   def initialize
-    @known_pairs
+    @known_pairs = []
   end
 
   # Takes the raw input, be it from a file or STDIN and preps the recommender.
   def parse_raw(lines)
     minimum = lines.shift.to_i
-    parse_logs(lines)
+    pairs = parse_logs(lines)
+    pretty_print(pairs)
   end
 
   # Walks through every line and attempts to parse it.
@@ -22,27 +23,44 @@ class BrandFactoryRecommender
       add_new_pairs(available_pairs)
     end
 
-    pairs
+    pair_counts = @known_pairs.uniq.map do |pair|
+      count = 0
+      pair_regex = /#{pair[0]},#{pair[1]}/
+      lines.each do |line|
+        count += 1 if line.match(pair_regex)
+      end
+      { pair: pair, count: count }
+    end
+
+    pair_counts = pair_counts.delete_if do |value|
+      value[:count] < 3
+    end
+
+    pair_counts
   end
 
   def add_new_pairs(pairs)
     pairs.each do |pair|
-      @known_pairs << pair unless @known_pairs.include? pair
+      @known_pairs << pair
     end
   end
 
   def get_pairs_from_tokens(line_factories)
-    pair_count = line_factories.count - 1
-    return [] if pair_count == 0
+    factories_count = line_factories.count
+    return [] if factories_count == 0
 
     pairs = []
-    until line_factories.empty?
+    until line_factories[1].nil?
       fetched_pair = [line_factories.shift, line_factories[0]]
-      break if fetched_pair[1].nil?
       pairs << fetched_pair
     end
 
     pairs
+  end
+  
+  def pretty_print(pairs)
+    strings = pairs.map { |pair| pair[:pair].sort.join',' }
+    strings.sort.each { |str| puts str }
   end
 end
 
